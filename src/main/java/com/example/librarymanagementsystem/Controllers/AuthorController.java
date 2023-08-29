93654a9c95fa69262e5eda7cc08c66399be81ede
Wajid <ansariwajid9999@gmail.com>
2023-08-30 03:22:20 +0530

    swagger added

--- src/main/java/com/example/librarymanagementsystem/Controllers/AuthorController.java
+++ src/main/java/com/example/librarymanagementsystem/Controllers/AuthorController.java
@@ -73,7 +73,6 @@ public class AuthorController {
         try{
             Author author=authorService.getAuthorByAuthorId(authorId);
             return new ResponseEntity(author,HttpStatus.ACCEPTED);
-
         }
         catch (Exception e){
             return new ResponseEntity(null,HttpStatus.BAD_REQUEST);
